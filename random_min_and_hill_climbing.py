6085d01075393f0ce8874f9c2a3371a9537eaaf1
EduardoHGomez <hurtado.josegomez@gmail.com>
2025-01-24 11:49:25 -0600

    random min added

--- random_min_and_hill_climbing.py
+++ random_min_and_hill_climbing.py
@@ -5,6 +5,9 @@ import random
 # Rastrigin
 # Styblinsky-Tang
 
+def hc_min(min_start, xlow, xhigh):
+	
+	return xmin
   
 # 𝑓(𝑥) = 100(𝑥2 − 𝑥1^2)^2 + (1 − 𝑥1)^2
 def rosenbrock(x): # Receiving an array
@@ -12,31 +15,10 @@ def rosenbrock(x): # Receiving an array
 	# x[1] = x2
 	return 100 * (x[1] - x[0]**2)** 2 + (1 - x[0])**2
 
-
-# Hill Climbing algorithm
-
-
-def hc_min(func, xlow, xhigh, n):
-	# Solucion aleatoria zona factible
-	xrand1 = random.randint(xlow[0], xhigh[0])
-	xrand2 = random.randint(xlow[1], xhigh[1])
-	x = [xrand1, xrand2] # Hacer el vector de valores aleatorios
-
-	# Evaluar la solucion
-	fitness = 1 / (1 + func([xrand1, xrand2]))
-
-	# Incremento aleatorio. delta_x el incremento maximo de x
-	# delta_x = [-0.5 * ]
-	
-	return xmin
-
-
-
-
 def random_min(func, xlow, xhigh, n):
     # Seleccionar el random minimo con n iteraciones
     least_x = None  # Almacenar el minimo x arreglo
-    least_value = float('inf')  # Inicia con infinito
+    least_value = float('inf')  # Inicia con infinito pues es como el valor maximo
 
     for i in range(n):
         x1 = random.uniform(xlow[0], xhigh[0])
@@ -53,6 +35,6 @@ def random_min(func, xlow, xhigh, n):
 if __name__ == '__main__':
 	xlow = (-5, -5)
 	xhigh = (5, 5)
-	xmin = hc_min(rosenbrock, xlow, xhigh, 100)
+	xmin = random_min(rosenbrock, xlow, xhigh, 100)
 	print(xmin)
 	
