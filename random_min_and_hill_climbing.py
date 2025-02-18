abf19ddb3c0560f5550f066d1b3435552af5cbe0
EduardoHGomez <hurtado.josegomez@gmail.com>
2025-02-17 23:05:59 -0600

    Finished

--- random_min_and_hill_climbing.py
+++ random_min_and_hill_climbing.py
@@ -1,19 +1,28 @@
 # First session: Make the following functions with hill climbing algorithm
 import random
+import math
 
 # Rosenbrock
 # Rastrigin
 # Styblinsky-Tang
 
 def hc_min(min_start, xlow, xhigh):
-	
-	return xmin
-  
+    pass
+
 # 𝑓(𝑥) = 100(𝑥2 − 𝑥1^2)^2 + (1 − 𝑥1)^2
 def rosenbrock(x): # Receiving an array
 	# x[0] = x1
 	# x[1] = x2
 	return 100 * (x[1] - x[0]**2)** 2 + (1 - x[0])**2
+
+# Rastrigin
+def rastrigin(x):
+    return 20 + (x[0]**2 - 20 * math.cos(2 * math.pi * x[0])) + (x[1]**2 - 20 * math.cos(2 * math.pi * x[1]))
+
+# Styblinski-Tang
+def styblinski_tang(x):
+    return 0.5 * ((x[0]**4 - 16 * x[0]**2 + 5 * x[0]) + (x[1]**4 - 16 * x[1]**2 + 5 * x[1]))
+
 
 def random_min(func, xlow, xhigh, n):
     # Seleccionar el random minimo con n iteraciones
@@ -33,8 +42,14 @@ def random_min(func, xlow, xhigh, n):
 
 
 if __name__ == '__main__':
-	xlow = (-5, -5)
-	xhigh = (5, 5)
-	xmin = random_min(rosenbrock, xlow, xhigh, 100)
-	print(xmin)
-	
+    xlow = (-5, -5)
+    xhigh = (5, 5)
+
+    n = 100
+    for i in range(3):
+        print(f"Rosenbrockr con {n} iteraciones", "x*:", random_min(rosenbrock, xlow, xhigh, n), "f(x*):", rosenbrock(random_min(rosenbrock, xlow, xhigh, n)))
+        print(f"Rastrigin con {n} iteraciones", "x*:", random_min(rastrigin, xlow, xhigh, n), "f(x*):", rastrigin(random_min(rastrigin, xlow, xhigh, n)))
+        print(f"Styblinski-Tang con {n} iteraciones", "x*:", random_min(styblinski_tang, xlow, xhigh, n), "f(x*):", styblinski_tang(random_min(styblinski_tang, xlow, xhigh, n)))
+        print('------------------')
+
+        n *= 100
